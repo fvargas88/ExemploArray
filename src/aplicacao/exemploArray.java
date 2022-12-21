b76faa60b27e8e4e709966a2be0af231546ff7da
fvargas88 <fvargas_88@hotmail.com>
2022-12-21 15:27:12 -0300

    Exercício exemplo Array

---
+++ src/aplicacao/exemploArray.java
@@ -0,0 +1,34 @@
+package aplicacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class exemploArray {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner (System.in);
+		
+		int n = sc.nextInt();
+		double [] vect = new double [n];
+		for (int i=0; i<n; i++) {
+			vect [i] = sc.nextDouble();
+		}
+		
+		double soma = 0.0;
+		for (int i=0; i<n; i++) {
+			soma += vect[i];
+			
+		}
+		
+		double media = soma/ n;
+		System.out.printf("A média da altura: %.2fn%", media);
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
